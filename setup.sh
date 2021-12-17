cdb4d15bde56d8bd9b24a86d0f5dcc0ac3ca2443
Talha Duran <talhaduran95@googlemail.com>
2021-12-17 20:24:22 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,9 @@ apt-get update -y
 apt-get install docker-ce docker-ce-cli containerd.io -y
 
 #docker-compose
+apt-get install curl -y
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 echo "done"
